ca577aa821114746fe3acac80272f63895b451d7
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-13 15:59:22 +0200

    update select

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,6 @@ st.title("Analyse des crypto-monnaies")
 st.write("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur issue des données de Binance. L'analyse technique ensuite générée permet de calculer les indicateurs utilisés et d'afficher des alertes sur ces indicateurs."
 )
 
-st.selectbox("Choisissez une crypto !", ["Bitcoin", "Ethereum"], "Bitcoin")
+st.selectbox(
+        "Choisissez une crypto !",
+        ("Bitcoin", "Ethereum", "BNB"))
