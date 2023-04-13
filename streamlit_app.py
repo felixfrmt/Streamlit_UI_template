17ad3a294b2cf35300462512aca09aa4f91c3e44
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-13 15:50:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ st.markdown(
 )
 st.title("Analyse des crypto-monnaies")
 
-st.text("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur issue des données de Binance. L'analyse technique ensuite générée permet de calculer les indicateurs utilisés et d'afficher des alertes sur ces indicateurs."
+st.write("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur issue des données de Binance. L'analyse technique ensuite générée permet de calculer les indicateurs utilisés et d'afficher des alertes sur ces indicateurs."
 )
