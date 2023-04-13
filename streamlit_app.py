c8c10b9aa7e24a34dabab1001bca0aa3911b75fd
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-13 16:14:18 +0200

    graph

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
 
 st.markdown(
     """
@@ -21,3 +22,23 @@ st.write("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur is
 st.selectbox(
         "Choisissez une crypto !",
         ("Bitcoin", "Ethereum", "BNB"))
+
+
+# Création des données de test
+data = {'Date': ['2022-01-01', '2022-01-02', '2022-01-03', '2022-01-04', '2022-01-05'],
+        'Open': [100, 110, 120, 130, 140],
+        'High': [120, 130, 140, 150, 160],
+        'Low': [90, 100, 110, 120, 130],
+        'Close': [110, 120, 130, 140, 150]}
+
+df = pd.DataFrame(data)
+
+# Création du graphique
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                                     open=df['Open'],
+                                     high=df['High'],
+                                     low=df['Low'],
+                                     close=df['Close'])])
+
+# Affichage du graphique dans Streamlit
+st.plotly_chart(fig)
