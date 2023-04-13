595b67110ae47a5f58ee78335d567f3c13cac254
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-13 15:36:21 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 
 
-st.header("Analyse crypto-monnaies")
+st.title("Analyse des crypto-monnaies")
 
-st.text("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur issue des données de Binance. L'analyse technique ensuite générée permet de calculer les indicateurs utilisés et d'afficher des alertes sur ces indicateurs.")
+st.text("Selectionnez une crypto-monnaie et visualisez le cours de sa valeur issue des données de Binance. L'analyse technique ensuite générée permet de calculer les indicateurs utilisés et d'afficher des alertes sur ces indicateurs.", width=None)
