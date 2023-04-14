98f08d05aea2096c06fd4e21996fe1dd1d5a3812
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-14 10:26:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,6 @@ components.html(
       '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">',
       height=0,
 )
-components.iframe("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css")
-
 
 st.markdown(
     """
