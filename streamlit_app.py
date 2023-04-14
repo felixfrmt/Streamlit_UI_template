607984c27590ed38cdbeca780d0b97bc126ef388
felixfrmt <97964741+felixfrmt@users.noreply.github.com>
2023-04-14 10:24:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,10 @@ import streamlit.components.v1 as components
 
 # bootstrap 4 collapse example
 # components.html(
-#     """
-#     <link href= crossorigin="anonymous">
-#     """,
+      '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">',
 #     height=0,
 # )
-components.iframe("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ")
+components.iframe("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css")
 
 
 st.markdown(
